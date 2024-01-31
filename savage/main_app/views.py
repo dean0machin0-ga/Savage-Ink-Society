59b7811b3b021acea788fd3c8de03a3a9542f0f3
dean0machin0 <dean0machin00@gmail.com>
2024-01-30 18:17:34 -0800

    About Django feature implemented

--- savage/main_app/views.py
+++ savage/main_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Home Page
 def home(request):
     return render(request, 'home.html')
+
+# About Page
+def about(request):
+    return render(request, 'about.html')
