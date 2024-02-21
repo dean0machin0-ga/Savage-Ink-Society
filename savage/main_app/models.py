3dfdd571e0d808ac59592c219a5441fe4e8657fa
dean0machin0 <dean0machin00@gmail.com>
2024-02-20 18:04:16 -0800

    Created user profile model importing djangos built-in user model

--- savage/main_app/models.py
+++ savage/main_app/models.py
@@ -1,5 +1,15 @@
 from django.db import models
-from .models import Profile
-
+from django.contrib.auth.models import User
+# from .models import Profile
 
 # Create your models here.
+
+# User Profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
