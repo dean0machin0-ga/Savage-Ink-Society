7ff017f4415ba3356fdfb75217b98845c1a6956e
dean0machin0 <dean0machin00@gmail.com>
2024-02-27 18:21:44 -0800

    Rendered a list/index of profiles on cards on the ink profile page

--- savage/main_app/models.py
+++ savage/main_app/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,6 +7,16 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
+    astrological_sign = models.CharField(max_length=50, blank=True)
 
+    @property
+    def age(self):
+        if self.birth_date:
+            today = date.today()
+            age = today.year - self.birth_date.year - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+            return age
+        else:
+            return None
+        
     def __str__(self):
         return self.user.username if self.user else "No User"
