ba3bed9795f0ad817ea580b60fb35c4e1135f577
dean0machin0 <dean0machin00@gmail.com>
2024-02-27 19:30:14 -0800

    Refactored the details.html to render the user and full name properly

--- savage/main_app/models.py
+++ savage/main_app/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    first = models.CharField(max_length=50, blank=True)
+    last = models.CharField(max_length=50, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
