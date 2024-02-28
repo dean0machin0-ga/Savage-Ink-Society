a630a2c5c20ff0152d03bd7b9f648dad9c9cb907
dean0machin0 <dean0machin00@gmail.com>
2024-02-27 16:40:51 -0800

    Auth user model added to profile model

--- savage/main_app/models.py
+++ savage/main_app/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-# from .models import Profile
 
-# Create your models here.
-
-# User Profile
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.username if self.user else "No User"
